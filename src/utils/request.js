7a45630ffc07565b74212e38801d04d35fa5445d
崔海鹏 <chpeng1108@163.com>
2024-01-13 20:29:58 +0800

    Revert "create-vue"

--- src/utils/request.js
+++
@@ -1,8 +0,0 @@
-// axios
-import axios from "axios";
-const request = axios.create({
-    baseURL:'http://localhost:5081',
-    withCredentials:true
-})
-//采用export default该方式
-export default request
